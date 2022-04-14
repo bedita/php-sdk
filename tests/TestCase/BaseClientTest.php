d2b577bf4c8619368cbb3eaebca8fa4c8e8a7a28
dante di domenico <dante@didoda.com>
2022-04-14 11:51:51 +0200

    feat: more tests

--- tests/TestCase/BaseClientTest.php
+++ tests/TestCase/BaseClientTest.php
@@ -478,4 +478,43 @@ class BaseClientTest extends TestCase
         static::assertEquals($expected['message'], $this->client->getStatusMessage());
         static::assertEmpty($response);
     }
+
+    /**
+     * Test `unsetAuthorization`
+     *
+     * @return void
+     * @covers ::unsetAuthorization()
+     */
+    public function testUnsetAuthorization(): void
+    {
+        // test unset on empty headers Authorization
+        $this->invokeMethod($this->client, 'unsetAuthorization', []);
+        $property = new \ReflectionProperty($this->client, 'defaultHeaders');
+        $property->setAccessible(true);
+        static::assertArrayNotHasKey('Authorization', $property->getValue($this->client));
+
+        // populate headers Authorization then test unset
+        $property->setValue($this->client, ['Authorization' => 'Bearer *****************']);
+        $this->invokeMethod($this->client, 'unsetAuthorization', []);
+        $property = new \ReflectionProperty($this->client, 'defaultHeaders');
+        $property->setAccessible(true);
+        static::assertArrayNotHasKey('Authorization', $property->getValue($this->client));
+    }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     * @return mixed Method return.
+     */
+    protected function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
 }
