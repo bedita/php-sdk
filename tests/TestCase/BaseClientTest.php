5224b526ef327eecd059def737d9c2914f988f60
dante di domenico <dante@didoda.com>
2024-09-05 09:48:32 +0200

    tests: restore wrong renew token test

--- tests/TestCase/BaseClientTest.php
+++ tests/TestCase/BaseClientTest.php
@@ -507,11 +507,10 @@ class BaseClientTest extends TestCase
                 ['authenticate' => false],
                 new \BadMethodCallException('You must be logged in to renew token'),
             ],
-            // FIXME: restore this test when BE 4.7 is released
-            // 'wrong renew token' => [
-            //     ['authenticate' => true, 'token' => 'gustavo' ],
-            //     new BEditaClientException('[401] Wrong number of segments', 401),
-            // ],
+            'wrong renew token' => [
+                ['authenticate' => true, 'token' => 'gustavo' ],
+                new BEditaClientException('[401] Wrong number of segments', 401),
+            ],
             'renew token as logged' => [
                 ['authenticate' => true],
                 [
